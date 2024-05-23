4e68e7db3e1885eda33813ae351f17d3c809187b
Hrushikesh <khandalehrushikesh2@gmail.com>
2024-05-23 23:42:25 +0530

    updated the server configurations

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const Data = require('./models/Data');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors());
