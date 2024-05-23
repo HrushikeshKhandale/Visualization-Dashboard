8163427eeb0dff60f4a4b2dc55ca190cd6fa8750
Hrushikesh <khandalehrushikesh2@gmail.com>
2024-05-23 23:17:38 +0530

    updated the server configurations

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,14 @@ const Data = require('./models/Data');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb://localhost:27017/data-visualization-dashboard" || process.env.MONGODB_URI )
     .then(() => {
         console.log('MongoDB connected...');
         // Seed the database
