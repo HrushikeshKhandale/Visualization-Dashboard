29dcf958f9c878cc21e1d6727203b5b279d34681
Hrushikesh <khandalehrushikesh2@gmail.com>
2024-05-23 21:53:48 +0530

    Integrated Dashboard within App.js File

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="container">
+                <h1 className="mt-4">Data Visualization Dashboard</h1>
+                <Dashboard />
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
